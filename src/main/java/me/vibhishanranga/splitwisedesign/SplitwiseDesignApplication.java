a7e6ef2d8d65bce4cbc05a9ae73e743a3e92427b
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2024-10-02 21:43:47 +0100

    project setup
    Signed-off-by: Vibhishan Ranga <vibhishan.ranga1410@gmail.com>

---
+++ src/main/java/me/vibhishanranga/splitwisedesign/SplitwiseDesignApplication.java
@@ -0,0 +1,13 @@
+package me.vibhishanranga.splitwisedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitwiseDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/vibhishanranga/splitwisedesign/SplitwiseDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vibhishanranga.splitwisedesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
